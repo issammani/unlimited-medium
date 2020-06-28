9ed81289868841d8f0832ad1ed0903e41583a265
issammani <issamouu69@gmail.com>
2020-06-28 16:19:32 +0100

    Add function to clear cookies
    - Add clearAllCookiesFromDomain to clear all cookies associated with
      medium.com domain.
    
    - Add global variable to keep track of medium tab id and url.
    
    - Refresh page after clearing cookie data.

--- src/js/background.js
+++ src/js/background.js
@@ -1,13 +1,28 @@
 const MEDIUM_URL = /.*:\/\/.*medium.com\/.*/;
 
-const handlePageActionClick = () => {
-    // DO SOMETHING...
+let CURRENT_TAB = {url: '', id: 0};
+
+// Clear all cookies associated with domain
+// This is how medium keeps track of the monthly counter
+const clearAllCookiesFromDomain = async domain => {
+    const cookies = await browser.cookies.getAll({domain: '.medium.com'});
+    for(let cookie of cookies){
+        console.log(cookie);
+        await browser.cookies.remove({name: cookie.name, url: CURRENT_TAB.url});
+    }
+};
+
+const handlePageActionClick = async () => {
+    await clearAllCookiesFromDomain('.medium');
+    await browser.tabs.reload(CURRENT_TAB.id);
 };
 
 browser.tabs.onActivated.addListener( async activeInfo => {
     // Get active tab
     const tab = await browser.tabs.get(activeInfo.tabId);
-        
+    CURRENT_TAB.url = tab.url;
+    CURRENT_TAB.id = tab.id;
+    
     // Check if active tab matches medium url
     if(MEDIUM_URL.test(tab.url)){
         // Activate pageAction icon

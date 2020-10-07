c079df21533996be74022a5bb22a901455263485
issamouu <issamouu69@gmail.com>
2020-10-07 22:22:59 +0200

    feat: add prettier

--- app/src/js/background.js
+++ app/src/js/background.js
@@ -1,29 +1,32 @@
 const MEDIUM_URL = /.*:\/\/.*medium.com\/.*/;
 
-let CURRENT_TAB = {url: '', id: 0};
+let CURRENT_TAB = { url: "", id: 0 };
 
 // Clear all cookies associated with domain
 // This is how medium keeps track of the monthly counter
-const clearAllCookiesFromDomain = async domain => {
-    const cookies = await browser.cookies.getAll({domain: '.medium.com'});
-    for(let cookie of cookies){
+const clearAllCookiesFromDomain = async (domain) => {
+    const cookies = await browser.cookies.getAll({ domain: ".medium.com" });
+    for (let cookie of cookies) {
         console.log(cookie);
-        await browser.cookies.remove({name: cookie.name, url: CURRENT_TAB.url});
+        await browser.cookies.remove({
+            name: cookie.name,
+            url: CURRENT_TAB.url,
+        });
     }
 };
 
 const handlePageActionClick = async () => {
-    console.log('Clicky');
-    await clearAllCookiesFromDomain('.medium');
+    console.log("Clicky");
+    await clearAllCookiesFromDomain(".medium");
     await browser.tabs.reload(CURRENT_TAB.id);
 };
 
-const handleActivated = async activeInfo => {
+const handleActivated = async (activeInfo) => {
     // Get active tab
     const tab = await browser.tabs.get(activeInfo.tabId);
 
     // Check if active tab matches medium url
-    if(MEDIUM_URL.test(tab.url)){
+    if (MEDIUM_URL.test(tab.url)) {
         // Keep track of the current tab
         CURRENT_TAB.url = tab.url;
         CURRENT_TAB.id = tab.id;
--- webpack.config.js
+++ webpack.config.js
@@ -6,51 +6,51 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const __app_dirname = (subdir) => __dirname + "/app/" + subdir;
 
 module.exports = {
-  entry: {
-    background: path.resolve(__app_dirname("src/js"), "background.js"),
-  },
-
-  output: {
-    filename: "js/[name].js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-    ],
-  },
-  plugins: [
-    new CopywebpackPlugin({
-      patterns: [
-        { from: __app_dirname("assets"), to: "assets" },
-        { from: __app_dirname("manifest.json"), to: "manifest.json" },
-      ],
-    }),
-    new MiniCssExtractPlugin({ filename: "css/[name].css" }),
-
-    // Since browsers don't allow usage of eval by default
-    // inject a prop into the manifest.json when in dev mode
-    // allowing us to explicitly enable the usage of eval
-    // We could change the devtool but eval has faster build times
-    function () {
-      this.plugin("done", (stats) => {
-        // Inject only in dev mode
-        if (stats.compilation.options.mode === "development") {
-          fs.readFile("./app/manifest.json", (err, data) => {
-            fs.writeFile(
-              "./dist/manifest.json",
-              JSON.stringify({
-                ...JSON.parse(data),
-                content_security_policy:
-                  "script-src 'self' 'unsafe-eval'; object-src 'self'",
-              }),
-              () => {}
-            );
-          });
-        }
-      });
+    entry: {
+        background: path.resolve(__app_dirname("src/js"), "background.js"),
     },
-  ],
+
+    output: {
+        filename: "js/[name].js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new CopywebpackPlugin({
+            patterns: [
+                { from: __app_dirname("assets"), to: "assets" },
+                { from: __app_dirname("manifest.json"), to: "manifest.json" },
+            ],
+        }),
+        new MiniCssExtractPlugin({ filename: "css/[name].css" }),
+
+        // Since browsers don't allow usage of eval by default
+        // inject a prop into the manifest.json when in dev mode
+        // allowing us to explicitly enable the usage of eval
+        // We could change the devtool but eval has faster build times
+        function () {
+            this.plugin("done", (stats) => {
+                // Inject only in dev mode
+                if (stats.compilation.options.mode === "development") {
+                    fs.readFile("./app/manifest.json", (err, data) => {
+                        fs.writeFile(
+                            "./dist/manifest.json",
+                            JSON.stringify({
+                                ...JSON.parse(data),
+                                content_security_policy:
+                                    "script-src 'self' 'unsafe-eval'; object-src 'self'",
+                            }),
+                            () => {}
+                        );
+                    });
+                }
+            });
+        },
+    ],
 };
